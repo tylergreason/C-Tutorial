b2cb6f30f3b2070f0f9f9bfdcaff763061a72137
tylergreason <tyler.greason@protonmail.com>
2020-12-11 09:56:31 -0500

    Finished first math section

---
+++ Tutorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tutorial
+{
+    class Program
+    {
+        static void WorkingWithIntegers()
+        {
+            int a = 18;
+            int b = 10;
+            int c = a + b;
+            double d = 1.1;
+            Console.WriteLine(c * d + b);
+            Console.WriteLine(a / b);
+            double e = a / b;
+            Console.WriteLine(e);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            WorkingWithIntegers();
+        }
+    }
+}
